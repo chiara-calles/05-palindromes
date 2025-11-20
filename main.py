77f191781a2703929e52d24622c13e5b1cee9c8f
chiara-calles <chiara.calles@edu.esiee.fr>
2025-11-20 09:25:35 +0000

    exercice palindromes

--- main.py
+++ main.py
@@ -1,17 +1,55 @@
+"""
+Vérifie si une chaîne de caractère est un palindrome
+"""
 #### Fonction secondaire
 
 
 def ispalindrome(p):
 
-    # votre code ici
-    
+    """
+    Nettoie la chainde caractère p en supprimant les espaces, accents et caractères spéciaux
+    et la compare avec son inverse
+    """
+    p = p.lower()# chaine en majuscule
+    p = p.replace(" ","")# supprime les espaces blancs
+
+    #supprime les accents
+    p = p.replace("é","e")
+    p = p.replace("è","e")
+    p = p.replace("ê","e")
+    p = p.replace("ë","e")
+    p = p.replace("à","a")
+    p = p.replace("â","a")
+    p = p.replace("ï","i")
+    p = p.replace("î","i")
+    p = p.replace("ô","o")
+    p = p.replace("û","u")
+    p = p.replace("ü","u")
+    p = p.replace("ç","c")
+
+    #supprime les caractères spéciaux
+    clean_p = ""
+    for char in p:
+        if char.isalnum():
+            clean_p = clean_p + char
+    p = clean_p
+
+    s = p[::-1]# on créer une chaine s qui est l'inverse de p
+
+    if p == s:
+        return True
+
+
     return False
 
 #### Fonction principale
 
 
 def main():
-
+    """
+    Fonction principale (main) qui teste la fonction
+    ispalindrome()
+    """
     # vos appels à la fonction secondaire ici
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
